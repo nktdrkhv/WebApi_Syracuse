45e2e68a4e4d47c3e0702348a6d240a1b996df44
Nikita Dorokhov <nktdrkhv@outlook.com>
2022-07-27 18:02:07 +0700

    More prettier output of Form data

--- Program.cs
+++ Program.cs
@@ -1,3 +1,7 @@
+using Microsoft.Extensions.Logging;
+
+using System.Text;
+
 var builder = WebApplication.CreateBuilder(args);
 
 var app = builder.Build();
@@ -6,17 +10,30 @@ app.UseHttpsRedirection();
 
 app.MapPost("/tilda", (context) =>
 {
-	var request = context.Request;
-	if (request.Form["test"] == "test")
+	var requestForm = context.Request.Form;
+	if (requestForm["test"] == "test")
 	{
 		app.Logger.LogInformation("External texting of a Webhook");
 		return context.Response.WriteAsync("test");
 	}
-
-	foreach (var input in context.Request.Form)
-		Console.WriteLine($"{input.Key} - {input.Value}");
-
+	PrintFormData(requestForm);
 	return Task.CompletedTask;
 });
 
+app.MapPost("/merchant", (context) =>
+{
+	var requestForm = context.Request.Form;
+	PrintFormData(requestForm);
+	return context.Response.WriteAsync("YES");
+});
+
 app.Run();
+
+void PrintFormData(IFormCollection form)
+{
+	var log = new StringBuilder();
+	foreach (var input in form)
+		_ = log.AppendLine($"{input.Key} - {input.Value}");
+	log[^1] = ' ';
+	app.Logger.LogInformation(log.ToString());
+}
