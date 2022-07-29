2209a8e9c93a401535c1230fb6ce1c13bf6d853c
Nikita Dorokhov <nktdrkhv@outlook.com>
2022-07-29 16:51:29 +0700

    Mapping, Validation, some calculations

--- Program.cs
+++ Program.cs
@@ -1,34 +1,38 @@
-using Microsoft.Extensions.Logging;
-
 using System.Text;
+using AutoMapper;
+using FluentValidation;
 
 var builder = WebApplication.CreateBuilder(args);
+
+var mappingConfig = new MapperConfiguration(mc => mc.AddProfile(new CustomerMapper()));
+IMapper autoMapper = mappingConfig.CreateMapper();
+
+builder.Services.AddSingleton(autoMapper);
+builder.Services.AddScoped<IValidator<Customer>, CustomerValidator>();
 
 var app = builder.Build();
 
 app.UseHttpsRedirection();
 
-// name, surname?, number, age, height, weight, gender, activity_level
-
-app.MapPost("/tilda", (context) =>
+app.MapPost("/tilda", async (HttpContext context, IValidator<Customer> validator, IMapper mapper) =>
 {
     app.Logger.LogDebug("Got a request");
     var requestForm = context.Request.Form;
+
     if (requestForm["test"] == "test")
     {
-        app.Logger.LogInformation("External texting of a Webhook");
+        app.Logger.LogInformation("External testing of a webhook");
         return context.Response.WriteAsync("test");
     }
+
     PrintFormData(requestForm);
+
+    var customer = mapper.Map<IFormCollection, Customer>(requestForm); //
+    var validationResult = await validator.ValidateAsync(customer); //
+
+    if (validationResult.IsValid)
+        app.Logger.LogInformation(customer.ToString());
     return Task.CompletedTask;
-});
-
-
-app.MapPost("/merchant", (context) =>
-{
-    var requestForm = context.Request.Form;
-    PrintFormData(requestForm);
-    return context.Response.WriteAsync("YES");
 });
 
 app.Run();
@@ -36,6 +40,7 @@ app.Run();
 void PrintFormData(IFormCollection form)
 {
     var log = new StringBuilder();
+    log.Append("Raw input data from a form:");
     foreach (var input in form)
         _ = log.AppendLine($"{input.Key} - {input.Value}");
     log[^1] = ' ';
